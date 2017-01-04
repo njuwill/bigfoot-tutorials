5851ab50c5a3dbf9b0b925a6d64e85090d35ae9a
Zongjun Hu <zongjunhu@yahoo.com>
2017-01-04 17:31:20 -0500

    add comments

--- rstudio/dplyr-test.R
+++ rstudio/dplyr-test.R
@@ -3,14 +3,20 @@ library(sparklyr)
 readRenviron("/usr/lib64/R/etc/Renviron")
 
 library(DBI)
+
+# load dplyr, a package to help data frame manipulation
 library(dplyr) 
 
+# replace USER_NAME with your bigfoot user name
 sc <- spark_connect(master = "yarn-client",version = "1.6.0", config = list(default = list(spark.yarn.principal = "USER_NAME@CLUSTER")))
 
+# select database
 dbGetQuery(sc, "USE hpcjob")
 
+# create table dataframe
 job_finish=tbl(sc,"job_finish")
 
+# create user job number report by projects
 results <- job_finish %>% 
     filter(username=="zhu") %>% 
     group_by(projectname) %>% 
@@ -18,4 +24,5 @@ results <- job_finish %>%
     select(projectname, count) %>% 
     collect
 
+# make barplot
 barplot(results[[2]], names.arg=results[[1]])
